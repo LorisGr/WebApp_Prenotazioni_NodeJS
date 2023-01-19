454b31c8d87ef2c98571a809f9d9a04110c335be
LorisGr <lorisgr195@gmail.com>
2023-01-19 21:59:08 +0100

    percorso URL base endpoint

--- app.ts
+++ app.ts
@@ -11,6 +11,11 @@ import {
 const app = express();
 const router = express.Router();
 
+// Added a route for the root path '/'
+router.get('/', (_req, res) => {
+  res.send('Welcome to the reservation API');
+});
+
 router.get('/reservations', getAllReservations);
 router.get('/reservations/date', getReservationsByDate);
 router.get('/reservations/fiscalCode', getReservationsByFiscalCode);
@@ -22,8 +27,4 @@ app.use(router);
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
-});
-
-app.get('/',(_req, res) => {
-  res.send('Hello World!')
 });
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
-import './App.css';
 import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
   const [reservations, setReservations] = useState([]);
   const [selectedDate, setSelectedDate] = useState('');
   const [fiscalCode, setFiscalCode] = useState('');
-  const [ticket, setTicket] = useState('');
   const [error, setError] = useState('');
 
   useEffect(() => {
     fetchReservations();
-  }, []);
+  }, [selectedDate, fiscalCode]);
 
   const fetchReservations = async () => {
     try {
       const response = await fetch(`/reservations?date=${selectedDate}&fiscalCode=${fiscalCode}`);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
       const data = await response.json();
       setReservations(data);
     } catch (err) {
-      setError(err);
+      setError(err.toString());
     }
   };
 
@@ -30,52 +32,59 @@ function App() {
         body: JSON.stringify({ date: selectedDate, fiscalCode }),
         headers: { 'Content-Type': 'application/json' },
       });
-      const data = await response.json();
-      setTicket(data.ticket);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
       fetchReservations();
     } catch (err) {
-      setError(err);
+      setError(err.toString());
     }
   };
 
   const deleteReservation = async (ticket) => {
     try {
-      await fetch(`/reservations?ticket=${ticket}`, {
+      const response = await fetch(`/reservations?ticket=${ticket}`, {
         method: 'DELETE',
       });
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
       fetchReservations();
     } catch (err) {
-        setError(err);
-        }
-        };
-        
-        return (
-        <div>
-        <form>
+      setError(err.toString());
+    }
+  };
+
+  return (
+    <div>
+      <form>
         <label>
-        Data:
-        <input type="date" value={selectedDate} onChange={e => setSelectedDate(e.target.value)} />
+          Data:
+          <input type="date" value={selectedDate} onChange={e => setSelectedDate(e.target.value)} />
         </label>
         <br />
         <label>
-        Codice fiscale:
-        <input type="text" value={fiscalCode} onChange={e => setFiscalCode(e.target.value)} />
+          Codice fiscale:
+          <input type="text" value={fiscalCode} onChange={e => setFiscalCode(e.target.value)} />
         </label>
         <br />
         <button onClick={createReservation}>Prenota</button>
-        </form>
-        {error && <p>{error}</p>}
-        <h2>Prenotazioni</h2>
-        <ul>
+      </form>
+        <div>
+        <p>{error}</p>
+      </div>
+      <h2>Prenotazioni</h2>
+      <ul>
         {reservations.map(reservation => (
-        <li key={reservation.ticket}>
-        {reservation.date} - {reservation.fiscalCode}
-        <button onClick={() => deleteReservation(reservation.ticket)}>Cancella prenotazione</button>
-        </li>
+          <li key={reservation.ticket}>
+            {reservation.date} - {reservation.fiscalCode}
+            <button onClick={() => deleteReservation(reservation.ticket)}>Cancella prenotazione</button>
+          </li>
         ))}
-        </ul>
-        </div>
-        );
-        }
-        
-        export default App;
+      </ul>
+    </div>
+  );
+};
+
+export default App;
+
